d29a9863029dcc9349b3ad8839f0f957d46bd8b5
Siddharthkumarrrrr <Siddharthkmarrrrr@gmail.com>
2024-01-22 02:14:55 +0530

    new things added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import "./home.css"
 import Navbar from '../../Components/Navbar' 
 import BitAlmuniImg from '../../Components/BitAlmuniImg'
+import Foot from '../../Components/Foot'
+import CustomSlider from '../../Components/CustomSlider'
+
 function Home() {
   return (
     <div>
     <Navbar/>
     <BitAlmuniImg/>
-    </div>
+    <div className="message">
+       The insitiute has given many eniginerss to make this Planet a better place.
+      </div>
+
+    
+    <div className="button_list">
+        <button>AlumiList</button>
+        <button>Click Here to Create Your Profile</button>
+      </div>
+      <CustomSlider/>
+      <Foot/>
+     </div>
+    
   )
 }
 
